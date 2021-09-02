101877bcbb20b35ee725f7199777fd29056c0cde
marinajulia <marinajulia707@gmail.com>
2021-09-02 15:17:04 -0300

    teste api

--- src/Controller/CadastroController.ts
+++ src/Controller/CadastroController.ts
@@ -1,23 +1,25 @@
-import {Request, Response } from 'express'; 
+import { Request, Response } from 'express';
+import knex from '../Database/Connection';
 
-export default{
-    async create(req: Request, res: Response){
-        const {nome, cpf} = req.body;
+export default {
+    async create(req: Request, res: Response) {
+        const { nome, cpf } = req.body;
         const id = 3;
-        const data = {id, nome, cpf};
-        return res.status(201).json({data:data});
+        const data = { id, nome, cpf };
+        return res.status(201).json({ data: data });
     },
 
-    async list(req: Request, res: Response){
-        var result =[{id:1, cpf:"12345678", nome:"José"}, {id:2, cpf:"12345678", nome:"Maria"}]
-        return res.status(200).json({data:result});
+    async list(req: Request, res: Response) {
+        var result = await knex('tab_cadastro').orderBy('nome');
+
+        return res.status(200).json({ data: result });
     },
 
-    async update(req: Request, res: Response){
+    async update(req: Request, res: Response) {
         const { nome, cpf } = req.body;
         const dataAlteracao = '30/05/2021 16:00';
-        const cadastro = {nome, cpf, dataAlteracao};
+        const cadastro = { nome, cpf, dataAlteracao };
         cadastro.nome = "jose da silva";
-        return res.status(200).json({data:cadastro});
+        return res.status(200).json({ data: cadastro });
     },
 }
